3c9812491e752a47a6f618b0aec4326b394753a4
bobbybrooksatx <brooks@mindsetatx.com>
2020-07-09 18:51:39 -0500

    google analytics via tag manager - fuck mix panel

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,10 +26,31 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-plugin-mixpanel',
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        apiToken: '1a26f7ded80d0464eb340c7e31b1c18e', // required
-        pageViews: 'all',
+        id: "GTM-MTFCVZR",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        //gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        dataLayerName: "BBATX-Data-Layer",
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: "Page View",
       },
     },
   ],
